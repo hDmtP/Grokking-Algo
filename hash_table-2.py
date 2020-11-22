912bd91b783ac7f70c6d46cee368cdae5a7c4d24
Pritam Dhara <65482473+hDmtP@users.noreply.github.com>
2020-11-22 11:18:09 +0530

    hash table as a phonebook

---
+++ hash_table-2.py
@@ -0,0 +1,9 @@
+phone_book = {}
+
+phone_book["Jeffry"] = 9901474589
+phone_book["Emergency"] = 100
+phone_book["Alex"] = 6235894174
+phone_book["Simon"] = 3258697413
+
+print(phone_book)
+print(phone_book["Emergency"])
