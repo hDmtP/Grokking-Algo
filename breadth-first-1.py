c1ba8b2daae96bf2f80b3e066f82637350d8b66e
Pritam Dhara <65482473+hDmtP@users.noreply.github.com>
2020-11-23 11:54:51 +0530

    breadth-first search

---
+++ breadth-first-1.py
@@ -0,0 +1,14 @@
+# BUilding a Graph
+
+graph = {}
+graph["me"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["tom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["tom"] = []
+graph["jonny"] = []
+
+print(graph)
+print(graph["me"])
